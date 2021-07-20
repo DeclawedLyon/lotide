d8c853de54305aad5023ad680ca5dbc8b1c6bacb
DeclawedLyon <declan.e.lyons@gmail.com>
2021-07-20 17:55:17 -0600

    Added eqArrays file to lotide

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    //console.log(actual, expected);
+    //console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
+    return true;
+  } else {
+    //console.log(actual, expected);
+    //console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
+    return false;
+  }
+};
+
+//assertEqual('Lighthouse Labs', 'Bootcamp');
+//assertEqual(1, 1);
+
+const eqArrays = function(arr1, arr2) {
+  //console.log(arr1, arr2)
+  let i = 0;
+  let truth = true;
+  while (i < arr1.length){
+    if (assertEqual(arr1[i], arr2[i])) {
+      truth = true;
+    } else if (!assertEqual(arr1[i], arr2[i])){
+      truth = false;
+    }
+    i++
+  }
+  console.log(truth);
+  
+}
+eqArrays([1, 2, 3], [1, 2, 3]) 
+eqArrays([1, 2, 3], [3, 2, 1])
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
