02939cc994abc0b860ca12276719577eaa6fbfbb
DeclawedLyon <declan.e.lyons@gmail.com>
2021-07-21 15:15:11 -0600

    created a countLetters function in Lotide

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
+};
+
+testArray = {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+
+  for (const letter of sentence) {
+      if (letter !== ' ') {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    } 
+  }
+  console.log(result);
+};
+
+result1 = countLetters("lighthouse in the house");
+assertEqual(result1, testArray)
