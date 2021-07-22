19adf46160f86b4fbf24a236e8d284ab38425c3a
DeclawedLyon <declan.e.lyons@gmail.com>
2021-07-22 16:50:33 -0600

    Implemented findKey function in lotide

---
+++ findKey.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
+};
+
+
+
+const findKey = (object, callback) => {
+  let result = '';
+
+  for (const key in object) {
+    if (callback(object[key])) {
+      console.log('Item: ', key, ' was found!');
+      console.log('Key match: ', object[key]);
+      return key
+    }
+  }
+}
+
+// const findKeyByValue = function(object, value) {
+//   let result = '';
+//   for (const key in object) {
+//       if (object[key] === value) {
+//         result = key;
+//     }
+//   }
+//   console.log(result);
+//   return result;
+// }
+
+const test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+const test2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+assertEqual(test1, 'noma');
+assertEqual(test2, 'Akaleri');
