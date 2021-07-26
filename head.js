799f9dc7b4933a6713672bb47541f1bd11225366
DeclawedLyon <declan.e.lyons@gmail.com>
2021-07-26 13:07:43 -0600

    created automated tests for head and assert equal in lotide

--- head.js
+++ head.js
@@ -1,15 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
-  let first = arr[0];
-  return first;
+  let value = arr[0];
+  if (arr[0] === undefined) {
+    value = ''
+    return value
+  }
+  return value;
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([5]), 5);
+module.exports = head;
