6241ae394ad0cd74ae7ec065b424eafcd1aec722
DeclawedLyon <declan.e.lyons@gmail.com>
2021-07-26 15:43:23 -0600

    updated tail test to run with mocha

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,4 +1,13 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse,Labs");
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse,Labs");
+
+describe('#tail', () => {
+  it('returns "Lighthouse,Labs" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse,Labs")
+  });
+});
